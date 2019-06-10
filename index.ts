4d0231e85ad393475ce9347ffaa7a63704a6abc9
amr taher <amrtaher1995@gmail.com>
2019-06-10 18:38:53 +0200

    using factory class

--- index.ts
+++ index.ts
@@ -1,13 +1,9 @@
 import * as puppeteer from 'puppeteer';
-import { FindAndClick, FindAndWrite, Command, executeCommand } from './command'
-import * as fs from 'fs';
-const commandsArray = JSON.parse(fs.readFileSync('./register.test.json') as any);
+import { TestFactory } from './testFactory';
 (async () => {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    for (let i = 0; i < commandsArray.length; i++) {
-        console.log('executing command: ', commandsArray[i]);
-        await executeCommand(commandsArray[i], page);
-        console.log('done executing a command');
-    }
+    const testFactory = new TestFactory(page);
+    testFactory.appendTest('useraddition' , './user-addition.test.json');
+    testFactory.executeTest('useraddition' , 'user-additon-file.txt');
 })();
